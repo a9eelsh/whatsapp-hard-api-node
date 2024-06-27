8409f1d94f21c2268d8f28846fbd2eedf20dc37a
Aseel sherwani <taiger43@gmail.com>
2024-06-27 04:45:04 +0300

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session'); // Adicione esta linha
 const flash = require('connect-flash');
 const exceptionHandler = require('express-exception-handler');
 const error = require('../api/middlewares/error');
@@ -12,13 +14,15 @@ const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // Configuração do middleware de sessão
-app.use(
+app.use(session({
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-//  cookie: { secure: process.env.NODE_ENV === 'production' }
-);
+  cookie: { secure: process.env.NODE_ENV === 'production' }
+}));
 
 app.use(flash());
 
